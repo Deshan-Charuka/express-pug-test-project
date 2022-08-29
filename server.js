d61a8d117d63b04afc9b20cd06671f59e17b7d7a
Deshan Charuka <d.c.0729439631@gmail.com>
2022-08-29 10:58:21 +0530

    Load a dynamic pug file

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', (req, res) => {
 	res.render('index');
 })
 
+app.get('/member/:name/ID/:id', (req, res) => {
+	const memberDetails = {
+		member: req.params.name,
+		id: req.params.id
+	}
+	res.render('member', memberDetails);
+});
+
+app.get('*', (req, res, next) => {
+	res.status(200).send('Sorry, page not found');
+	next();
+});
+
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
